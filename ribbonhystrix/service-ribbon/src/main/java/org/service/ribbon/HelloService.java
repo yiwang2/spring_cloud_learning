9019579934a89157836d7a2dce5ee839c0bd100d
yiwang2 <lixinandme@gmail.com>
2019-11-22 10:06:10 +0000

    add circuit breaker Hystrix

---
+++ ribbonhystrix/service-ribbon/src/main/java/org/service/ribbon/HelloService.java
@@ -0,0 +1,23 @@
+package org.service.ribbon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@Service
+public class HelloService {
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	@HystrixCommand(fallbackMethod="hiError")
+	public String hiService(String name) {
+		return this.restTemplate.getForObject("http://service-hi/hi?name="+name, String.class);
+	}
+
+	public String hiError(String name) {
+		return "hi," + name + ",sorry,error!";
+	}
+}
