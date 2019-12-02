30ee6331e498bde95086dbdbd434bfccf18fae35
yiwang2 <lixinandme@gmail.com>
2019-12-02 09:34:22 +0000

    hystrix + turbine

---
+++ spring-cloud-learning-hystrix-turbine/service-hi/src/main/java/org/service/hi/ServiceHiApplicationController.java
@@ -0,0 +1,24 @@
+package org.service.hi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@RestController
+public class ServiceHiApplicationController {
+	@Value("${server.port}")
+	String port;
+
+	@RequestMapping("/hi")
+	@HystrixCommand(fallbackMethod = "hiError")
+	public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+		return "hi " + name + " ,i am from port:" + port;
+	}
+
+	public String hiError(String name) {
+		return "hi," + name + ",sorry,error!";
+	}
+}
---
+++ spring-cloud-learning-hystrix-turbine/service-lucy/src/main/java/org/service/lucy/ServiceLucyApplicationController.java
@@ -0,0 +1,26 @@
+package org.service.lucy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@RestController
+public class ServiceLucyApplicationController {
+
+	
+	@Value("${server.port}")
+	String port;
+
+	@RequestMapping("/hi")
+	@HystrixCommand(fallbackMethod = "hiError")
+	public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+		return "hi " + name + " ,i am from port:" + port;
+	}
+
+	public String hiError(String name) {
+		return "hi," + name + ",sorry,error!";
+	}
+}
